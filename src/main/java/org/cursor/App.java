0450b2947cf8ef3ec3bbc9a15a3af4e1a682bad3
AndriiTysiak <tysiak45@gmail.com>
2022-10-15 15:51:13 +0300

    initial commit

---
+++ src/main/java/org/cursor/App.java
@@ -0,0 +1,17 @@
+package org.cursor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Car car = new Car();
+        car.setModel("BMW");
+        car.setAge(4);
+        System.out.println(car);
+
+    }
+}
